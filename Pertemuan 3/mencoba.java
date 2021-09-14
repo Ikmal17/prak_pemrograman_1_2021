60767635bec9ffb51b244a7e4f6e3de6be4c26b3
Ikmal Dwi Darmawan <ikmaldwi.2021@student.uny.ac.id>
2021-09-14 14:04:21 +0700

    mencoba variabel java  tambahan integer

--- Pertemuan 3/mencoba.java
+++ Pertemuan 3/mencoba.java
@@ -1,5 +1,3 @@
-import java.lang.ref.Cleaner;
-
 public class mencoba {
     public static void main (String[] args) {
         String nama_saya = "Dwi Darmawan Ikmal";
@@ -8,7 +6,7 @@ public class mencoba {
         double tinggiBadan = 178;
         System.out.println(nama_saya);
         System.out.println(namaSaya);
-        System.out.println(tinggiBadan);
         System.out.println(umur);
+        System.out.println("Tinggi Badan " +(int) tinggiBadan);
     }
 }
