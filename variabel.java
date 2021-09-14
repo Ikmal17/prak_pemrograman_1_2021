299fda7916eef2e250ea5edc52d71b497f231581
Ikmal Dwi Darmawan <ikmaldwi.2021@student.uny.ac.id>
2021-09-14 13:45:44 +0700

    mencoba variabel java revisi

---
+++ variabel.java
@@ -0,0 +1,13 @@
+public class variabel {
+    public static void main (String[] args) {
+        String nama_saya = "Dwi Darmawan Ikmal";
+        String namaSaya = "Ikmal Dwi Daramawan";
+        int umur = 18;
+        double tinggiBadan = 178;
+
+        System.out.println(nama_saya);
+        System.out.println(namaSaya);
+        System.out.println(tinggiBadan);
+        System.out.println(umur);
+    }
+}
