e497fd2957cfeb8fa9750eb3ae3de4fd1e13f65e
Ikmal Dwi Darmawan <ikmaldwi.2021@student.uny.ac.id>
2021-09-14 13:10:56 +0700

    variabel penamaan pada java

--- Pertemuan 2/Hello.java
+++ Pertemuan 2/Hello.java
@@ -1,4 +1,9 @@
+/* 
+Nama : Ikmal Dwi Darmawan
+NIM  : 21537144
+*/
 public class Hello {
+    // yuhuuuuuuuu
     public static void main(String[] args) {
         System.out.println("Pemrograman sangat menyenangkan");
     }
