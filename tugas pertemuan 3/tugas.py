1a5ccae186467c97ea637c49d5cd1228ab4c72c0
Ikmal Dwi Darmawan <ikmaldwi.2021@student.uny.ac.id>
2021-09-20 00:06:50 +0700

    TUGAS PYTHON PERTEMUAN KE 3

---
+++ tugas pertemuan 3/tugas.py
@@ -0,0 +1,47 @@
+print("====== BIODATA ======")
+
+print("")
+
+print("bismillahhirrohmannirrohim, semoga terwujud Amiin")
+
+print("")
+
+Nama = "Ikmal Dwi Darmawan"
+
+print("NAMA      : ", Nama)
+
+nim = 21537144025
+
+print("NIM       : ", nim)
+
+angkatan = 2021
+
+print("ANGKATAN  : ", angkatan)
+
+program_studi = "Teknologi Informasi"
+
+print("PRODI     : ", program_studi)
+
+jurusan = "Teknologi Informasi"
+
+print("JURUSAN   : ", jurusan)
+
+fakultas = "Teknik"
+
+print("FAKULTAS  : ", fakultas)
+
+ipk = 3.9
+
+print("IPK       : ", ipk)
+
+print("")
+
+print(" ..")
+
+print("----")
+
+print("")
+
+print(":)")
+
+print("")
