1d488bc544114e909f701c993980a0c106c81d4d
Ikmal Dwi Darmawan <ikmaldwi.2021@student.uny.ac.id>
2021-09-20 00:27:50 +0700

    TUGAS JAVA PERTEMUAN KE 3

---
+++ tugas pertemuan 3/tugas.java
@@ -0,0 +1,18 @@
+public class tugas {
+    public static void main(String[] args) {
+        System.out.println("NAMA      : IKMAL DWI DARMAWAN");
+
+        int var1;
+        long var2;
+
+        var1 = 2021;
+        var2 = 21537144025L;
+
+        System.out.println("NIM       : "+ var2);
+        System.out.println("ANGKATAN  : "+ var1);
+        System.out.println("PRODI     : Teknologi informasi");
+        System.out.println("JURUSAN   : Teknologi informasi");
+        System.out.println("FAKULTAS  : Teknik");
+        System.out.println("IPK       : 3.9");
+    }
+}
