1539a5f0e838a9523ddd6e28dd38cccb59f80984
Ikmal Dwi Darmawan <ikmaldwi.2021@student.uny.ac.id>
2021-11-30 15:41:59 +0700

    Pertemuan 2 revisi

---
+++ Pertemuan 2/LangkahA.java
@@ -0,0 +1,9 @@
+public class LangkahA {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // coba coba bro
+        /* coba komentar
+        bawah atas bro*/
+        System.out.println("Ikmal Dwi Darmawan");
+    }
+}
