f7802cbdea2bbceec8d0161176c47d2c1b1c77ce
Ikmal Dwi Darmawan <ikmaldwi.2021@student.uny.ac.id>
2021-11-30 20:49:08 +0700

    Belajar python input dan operator update

---
+++ Pertemuan 4/input.py
@@ -0,0 +1,20 @@
+# episode input user
+print("ikmal", end=" ")
+print("lamki")
+# data yang dimasukkan pasti string
+Nama = input("masukan nama : ")
+
+print("Nama saya adalah ", Nama, ", type = ", type (Nama))
+
+# jika ingin mengambil integer, maka
+variabel = float(input("masukan umur anda : "))
+angka = int(input("masukan berat badan anda : "))
+
+print("Umur saya adalah ", variabel, ", type = ", type (variabel))
+print("Berat badan saya adalah ", angka, ", type = ", type (angka))
+
+# jika ingin mengambil boolean
+biner = bool(int(input(" masukan data boolean : ")))
+
+print("data ", biner, ", type = ", type (biner))
+
